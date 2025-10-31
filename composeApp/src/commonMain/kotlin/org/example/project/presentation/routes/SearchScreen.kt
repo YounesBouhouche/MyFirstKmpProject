f42c588024e6ed651e719b4fc4b0c667f7e0e474
Younes <younes.bouhouche12@gmail.com>
2025-10-31 09:09:16 +0100

    Fixed picture disappearing without animation

--- composeApp/src/commonMain/kotlin/org/example/project/presentation/routes/SearchScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/presentation/routes/SearchScreen.kt
@@ -28,9 +28,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -56,13 +58,18 @@ fun SearchScreen(
         mutableStateOf("")
     }
     val selectedPicture by viewModel.selectedPicture.collectAsState()
+    var picture by remember { mutableStateOf(selectedPicture) }
+    LaunchedEffect(selectedPicture) {
+        if (selectedPicture != null)
+            picture = selectedPicture
+    }
     val clipboard = LocalClipboardManager.current
     val downloadState by viewModel.downloadState.collectAsState()
     AdaptiveNavigationContainer(
         Modifier.fillMaxSize(),
         showSecondPan = selectedPicture != null,
         secondPan = {
-            selectedPicture?.let { picture ->
+            picture?.let { picture ->
                 Column(
                     Modifier.background(MaterialTheme.colorScheme.surfaceContainer)
                         .padding(bottom = 24.dp)
